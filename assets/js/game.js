9e424aee2051848ab7c38e8e0a428cb164341a06
Nick Duran <nvduran@gmail.com>
2020-12-20 15:47:37 -0600

    .1.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("this is a test");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+console.log(10 + 20);
+
+console.log("Our robot's name is " +playerName);
+
+function fight() {
+    window.alert("The fight has begun");
+}
+
+//fight();
